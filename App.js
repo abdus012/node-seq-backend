67e9481a29813e32ee007f82aab26e27d7682bd1
abdus012 <51002552+abdus012@users.noreply.github.com>
2022-08-19 02:43:04 +0530

    Updates

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-// const express = require("express");
-// const app = express();
+
 const db = require('./seqDB/index')
 const Joi = require('joi');
 const config = require('./config/config')
 const manifest = config.server;
 const Glue = require('glue');
-// const { Client } = require('pg')
-// const client = new Client({
-//   user: 'myuser',
-//   host: 'localhost',
-//   database: 'employee',
-//   password: 'mypass',
-//   port: 5432,
-//   dialect: 'postgres'
-// })
-// client.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
 
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const fs = require('fs');
+
 const init = async () => {
 
     const server = Hapi.Server({
@@ -49,18 +35,3 @@ init();
 
 
 
-
-
-// start() 
-
-
-// const DB =
-//   "mongodb+srv://Abdus:xBUd9PlLBgbVVCQE@cluster0.yajlmt9.mongodb.net/mernstack-e-shop?retryWrites=true&w=majority";
-
-// MongoClient.connect(DB)
-//   .then((result) => {
-//     console.log("Connected");
-//   })
-//   .catch((err) => {
-//     console.log(err, "err");
-//   });
