63231d5b2bee033b36911f50b71b8571cefb35b5
abdus012 <abdussayyed1@gmail.com>
2022-08-22 17:42:07 +0530

    changes db chan

--- seqDB/index.js
+++ seqDB/index.js
@@ -3,11 +3,38 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
-const config = require('../config/config')['production'];
+const configJson = require('../config/config');
 const db = {};
 
-const sequelize = new Sequelize('','', '', config);
-console.log(config);
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+const config = configJson[env];
+
+let sequelize;
+if (config.environment === 'production') {
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable], config,
+  );
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      dialectOption: {
+        ssl: true,
+        native: true,
+      },
+      logging: true,
+    },
+  );
+} else {
+  sequelize = new Sequelize('','', '', config);
+
+}
+
+
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
