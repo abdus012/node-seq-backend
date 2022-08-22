88f865f60c7ffdaf0c52160673c1475a9585adfe
abdus012 <abdussayyed1@gmail.com>
2022-08-22 17:17:23 +0530

    changes db change

--- config/config.js
+++ config/config.js
@@ -1,18 +1,14 @@
 const config = {
   storage: {
-    production: {
-        use_env_variable: "DATABASE_URL",
+    development: {
+      username: "myuser",
+      host: "localhost",
+      database: "employee",
+      password: "mypass",
+      port: 5432,
       dialect: "postgres",
-      },
     },
-    // production: {
-    //   username: "myuser",
-    //   host: "localhost",
-    //   database: "employee",
-    //   password: "mypass",
-    //   port: 5432,
-    //   dialect: "postgres",
-    //  }
+  },
 };
 
 module.exports = config;
