d01454363ab635c02808e56836d7fcd0d001e778
abdus012 <51002552+abdus012@users.noreply.github.com>
2022-08-29 01:05:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const baseRoutes = {
   version: "1.0.0",
   register: async function (server, options) {
     server.route([
-      {
+     
     config: {
         cors: {
             origin: ['*'],
